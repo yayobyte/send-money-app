487695e964f4bcc8ce27af35cbd673514d00f035
yayobsf <yayobyte@gmail.com>
2024-01-06 21:19:54 +0400

    Create Acordeon

--- components/ExchangeRateFees.tsx
+++ components/ExchangeRateFees.tsx
@@ -1,44 +1,75 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
-
-import React from "react";
-import { theme } from "../config/ThemeContext";
+import React, { useState, useEffect } from "react"
+import { Text, View, StyleSheet, LayoutAnimation, UIManager, Platform, Pressable } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
+import { theme } from "../config/ThemeContext"
 
 type ExchangeRateFeesProps = {
   value: number
   from: string
   to: string
+};
+
+// Enable LayoutAnimation on Android
+if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true)
 }
 
 export const ExchangeRateFees = ({ from, to, value }: ExchangeRateFeesProps) => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false)
+  
+  useEffect(() => {
+    LayoutAnimation.easeInEaseOut()
+  }, [isAccordionOpen])
+  
+  const toggleAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen)
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.rate}>
-        <Text style={styles.bold}>{`1 `}</Text>
-        <Text style={styles.from}>{from}</Text>
-        <Text>{' = '}</Text>
-        <Text>{`${to} `}</Text>
-        <Text style={styles.bold}>{value}</Text>
-      </Text>
-      <TouchableOpacity style={styles.selector}>
-        <Text style={styles.dropdown}>Fees</Text>
-        <Ionicons name="chevron-down" size={theme.spacing.lg} color={theme.colors.primary} style={styles.icon}/>
-      </TouchableOpacity>
-    </View>
+    <Pressable style={styles.container} onPress={toggleAccordion} >
+      <View style={styles.exchangeRate}>
+        <Text style={styles.rate}>
+          <Text style={styles.bold}>{`1 `}</Text>
+          <Text style={styles.from}>{from}</Text>
+          <Text>{` = `}</Text>
+          <Text>{`${to} `}</Text>
+          <Text style={styles.bold}>{value}</Text>
+        </Text>
+        
+        <View style={styles.selector} >
+          <Text style={styles.dropdown}>Fees</Text>
+          <Ionicons
+            name={isAccordionOpen ? "chevron-up" : "chevron-down"}
+            size={theme.spacing.lg}
+            color={theme.colors.primary}
+            style={styles.icon}
+          />
+        </View>
+      </View>
+      
+      {isAccordionOpen && (
+        <View style={styles.additionalContent}>
+          <Text style={styles.fees}>+ <Text style={{ fontWeight: 'bold'}}>25</Text> AED</Text>
+        </View>
+      )}
+    </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "column",
     backgroundColor: theme.colors.highlight,
     marginVertical: theme.spacing.md,
     padding: theme.spacing.lg,
     borderRadius: theme.spacing.md,
   },
-  rate: {
+  exchangeRate: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rate: {
+    flexDirection: "row",
     color: theme.colors.lightText,
     fontSize: theme.fontSize.regular,
   },
@@ -47,15 +78,24 @@ const styles = StyleSheet.create({
   },
   dropdown: {
     color: theme.colors.primary,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   selector: {
+    flexDirection: "row",
+  },
+  additionalContent: {
     flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: theme.spacing.sm,
   },
   bold: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   from: {
     fontSize: theme.fontSize.md,
+  },
+  fees: {
+    color: theme.colors.lightText,
+    fontSize: theme.fontSize.regular,
   },
 })
